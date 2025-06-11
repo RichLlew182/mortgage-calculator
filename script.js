264a397bfb07e57606c45ebf7a2d15a28449b926
Richard Llewellyn <richard.llewellyn@webdadi.com>
2025-06-11 16:44:57 +0100

    added initial function that captures the data from the form

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+const mortgageCalculatorForm = document.querySelector('#mortgageCalculatorForm');
+
+mortgageCalculatorForm.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+    console.log(e);
+
+    const formData = new FormData(e.target);
+
+    console.log(formData)
+
+    const mortgageAmount = formData.get('mortgage-amount')
+    const mortgageTerm = formData.get('mortgage-term')
+    const interestRate = formData.get('interest-rate')
+    const mortgageType = formData.get('mortgage-type')
+
+    console.log('Mortgage Amount: ' + mortgageAmount);
+    console.log('Mortgage Term: ' + mortgageTerm);
+    console.log('Interest Rate: ' + interestRate);
+    console.log('Mortgage Type: ' + mortgageType);
+
+})
